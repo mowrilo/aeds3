6b0cea41e1243b5453691cfb6ad395367089682d
murilo <murilovfm@gmail.com>
2016-08-26 12:39:13 -0300

    upload kmp completo

--- kmp.c
+++ kmp.c
@@ -23,20 +23,44 @@ int* calc_pi(char *padrao, int m)
 	return pi;
 }
 
-
+void kmp(char *texto, char *padrao, int m, int n, int *pi)
+{
+	int i,j;
+	j=0;
+	for (i=0; i<n; i++)
+	{
+		while((j>0) && (texto[i] != padrao[j]))
+		{
+			j = pi[j];
+		}
+		if(texto[i] == padrao[j])
+		{
+			j++;
+		}
+		if(j == (m))
+		{
+			printf("casamento na posicao %d\n",(i-m+1));
+		}
+	}
+}
 
 int main (int argc, char *argv[])
 {
-	int n;
+	char *texto = "ajvnjdsnvdsvpipk";
+//	char *padrao = "ababbaba";
+//	int m = strlen(padrao);
+	int n = strlen(texto);
+//	scanf("%s",padrao);
 	char *padrao;
 	scanf("%s",padrao);
 	int m = strlen(padrao);
 	int *pi = calc_pi(padrao,m);
-	for (int n=0;n<m;n++)
+	/*for (int n=0;n<m;n++)
 	{
 		printf("%d ",pi[n]);
 	}
-	printf("\n");
+	printf("\n");*/
+	kmp(texto,padrao,m,n,pi);
 	free(pi);
 	return 0;
 }
